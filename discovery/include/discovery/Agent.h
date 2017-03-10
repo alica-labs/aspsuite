7fdff2cbb99f4f656fe093af5e208226fa62e1a2
Stephan Opfer <opfer@vs.uni-kassel.de>
2017-03-10 18:00:19 +0100

    - first test snippets for sending udp multicast - send and receive methods available, but not working - error is in send method currently

--- discovery/include/discovery/Agent.h
+++ discovery/include/discovery/Agent.h
@@ -2,8 +2,10 @@
 
 #include "discovery/Worker.h"
 
+#include <zmq.h>
 #include <uuid/uuid.h>
 #include <signal.h>
+
 
 namespace discovery
 {
@@ -11,7 +13,7 @@ namespace discovery
 class Agent : public Worker
 {
   public:
-    Agent();
+    Agent(std::string name, bool sender);
     virtual ~Agent();
 
     void run();
@@ -20,9 +22,20 @@ class Agent : public Worker
     static bool operating;
 
   private:
+    void setupReceiveUDPMulticast();
+    void setupSendUDPMulticast();
+    void send();
+    int msg_send (zmq_msg_t *msg_, void *s_, const char* group_, const char* body_);
+    void receive();
+    int msg_recv_cmp (zmq_msg_t *msg_, void *s_, const char* group_, const char* body_);
     void testUUIDStuff();
     void checkZMQVersion();
     uuid_t uuid;
+
+    // zmq stuff
+    bool sender;
+    void *ctx;
+    void *socket;
 };
 
 } /* namespace discovery */
--- discovery/src/Agent.cpp
+++ discovery/src/Agent.cpp
@@ -2,8 +2,10 @@
 
 #include <zmq.h>
 
+#include <assert.h>
 #include <chrono>
 #include <iostream>
+#include <string.h>
 #include <unistd.h>
 
 namespace discovery
@@ -11,25 +13,167 @@ namespace discovery
 
 bool Agent::operating = true;
 
-Agent::Agent() : Worker("Bob")
+Agent::Agent(std::string name, bool sender)
+    : Worker(name)
+    , sender(sender)
+    , socket(nullptr)
 {
     // generate
     uuid_generate(this->uuid);
+
+    // zmq stuff
+    this->ctx = zmq_ctx_new();
+    assert(ctx);
+
+    if (sender)
+    {
+        this->setupSendUDPMulticast();
+    }
+    else
+    {
+        this->setupReceiveUDPMulticast();
+    }
+}
+
+void Agent::setupSendUDPMulticast()
+{
+    this->socket = zmq_socket(ctx, ZMQ_DISH);
+
+    int rc = zmq_bind(socket, "udp://239.0.0.1:5555");
+    if (rc != 0)
+    {
+    	std::cout << "Error: " << zmq_strerror(errno) << std::endl;
+        assert(rc == 0);
+    }
+}
+
+void Agent::setupReceiveUDPMulticast()
+{
+    this->socket = zmq_socket(ctx, ZMQ_RADIO);
+
+    // Joining
+    int rc = zmq_join(socket, "Movies");
+    if (rc != 0)
+    {
+        strerror(errno);
+        assert(rc == 0);
+    }
+
+    // Connecting
+    rc = zmq_connect(socket, "udp://239.0.0.1:5555");
+    if (rc != 0)
+    {
+        std::cout << "Error: " << zmq_strerror(errno) << std::endl;
+        assert(rc == 0);
+    }
 }
 
 Agent::~Agent()
 {
+    int rc = zmq_close(socket);
+    if (rc != 0)
+    {
+        strerror(errno);
+        assert(rc == 0);
+    }
+
+    rc = zmq_ctx_term(this->ctx);
+    if (rc != 0)
+    {
+        strerror(errno);
+        assert(rc == 0);
+    }
 }
 
 void Agent::run()
 {
-	this->checkZMQVersion();
-	this->testUUIDStuff();
+    // this->checkZMQVersion();
+    // this->testUUIDStuff();
+
+    if (this->sender)
+    {
+        send();
+    }
+    else
+    {
+        receive();
+    }
+}
+
+void Agent::send()
+{
+    zmq_msg_t msg;
+    int rc = msg_send(&msg, this->socket, "Movies", "Godfather");
+    std::cout << "Sended " << rc << " chars." << std::endl;
+    std::cout << "Error: " << zmq_strerror(errno) << std::endl;
+    assert(rc == 9);
+}
+
+void Agent::receive()
+{
+    zmq_msg_t msg;
+    int rc = msg_recv_cmp(&msg, this->socket, "Movies", "Godfather");
+    std::cout << "Received " << rc << " chars." << std::endl;
+    std::cout << "Error: " << zmq_strerror(errno) << std::endl;
+    assert(rc == 9);
+}
+
+int Agent::msg_send(zmq_msg_t *msg_, void *s_, const char *group_, const char *body_)
+{
+    int rc = zmq_msg_init_size(msg_, strlen(body_));
+    if (rc != 0)
+        return rc;
+
+    memcpy(zmq_msg_data(msg_), body_, strlen(body_));
+
+    rc = zmq_msg_set_group(msg_, group_);
+    if (rc != 0)
+    {
+        zmq_msg_close(msg_);
+        return rc;
+    }
+
+    rc = zmq_msg_send(msg_, s_, 0);
+
+    zmq_msg_close(msg_);
+
+    return rc;
+}
+
+int Agent::msg_recv_cmp(zmq_msg_t *msg_, void *s_, const char *group_, const char *body_)
+{
+    int rc = zmq_msg_init(msg_);
+    if (rc != 0)
+        return -1;
+
+    int recv_rc = zmq_msg_recv(msg_, s_, 0);
+    if (recv_rc == -1)
+        return -1;
+
+    if (strcmp(zmq_msg_group(msg_), group_) != 0)
+    {
+        zmq_msg_close(msg_);
+        return -1;
+    }
+
+    char *body = (char *)malloc(sizeof(char) * (zmq_msg_size(msg_) + 1));
+    memcpy(body, zmq_msg_data(msg_), zmq_msg_size(msg_));
+    body[zmq_msg_size(msg_)] = '\0';
+
+    if (strcmp(body, body_) != 0)
+    {
+        zmq_msg_close(msg_);
+        return -1;
+    }
+
+    zmq_msg_close(msg_);
+    free(body);
+    return recv_rc;
 }
 
 void Agent::testUUIDStuff()
 {
-	uuid_generate(this->uuid);
+    uuid_generate(this->uuid);
 
     // unparse (to string)
     char uuid_str[37]; // ex. "1b4e28ba-2fa1-11d2-883f-0016d3cca427" + "\0"
@@ -63,37 +207,47 @@ void Agent::testUUIDStuff()
 
 void Agent::checkZMQVersion()
 {
-	int major, minor, patch;
-	zmq_version(&major, &minor, &patch);
-	std::cout << "Major: " << major << " Minor: " << minor << " Patch: " << patch << std::endl;
+    int major, minor, patch;
+    zmq_version(&major, &minor, &patch);
+    std::cout << "Major: " << major << " Minor: " << minor << " Patch: " << patch << std::endl;
 }
 
 void Agent::sigIntHandler(int sig)
 {
-	operating = false;
+    operating = false;
 }
 }
 
 int main(int argc, char **argv)
 {
-	discovery::Agent* bob = new discovery::Agent();
+    discovery::Agent *agent;
+    if (argc > 1 && strcmp(argv[1], "sender") == 0)
+    {
+        std::cout << "Creating sending agent!" << std::endl;
+        agent = new discovery::Agent("Sender", true);
+    }
+    else
+    {
+        std::cout << "Creating receiving agent!" << std::endl;
+        agent = new discovery::Agent("Receiver", false);
+    }
 
-	bob->setDelayedStartMS(std::chrono::milliseconds(0));
-	bob->setIntervalMS(std::chrono::milliseconds(1000));
+    agent->setDelayedStartMS(std::chrono::milliseconds(0));
+    agent->setIntervalMS(std::chrono::milliseconds(1000));
 
-	bob->start();
+    agent->start();
 
     // register ctrl+c handler
     signal(SIGINT, discovery::Agent::sigIntHandler);
 
     // start running
-    while(discovery::Agent::operating)
+    while (discovery::Agent::operating)
     {
-    	sleep(1);
+        sleep(1);
     }
 
-    bob->stop();
+    agent->stop();
 
-	delete bob;
+    delete agent;
     return 0;
 }

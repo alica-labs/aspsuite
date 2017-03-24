e8b53fb58c59552d9052ca92eec66686667c0f21
Stephan Opfer <opfer@vs.uni-kassel.de>
2017-03-24 16:07:59 +0100

    - nothing done

--- discovery/src/Agent.cpp
+++ discovery/src/Agent.cpp
@@ -177,33 +177,6 @@ void Agent::send()
 
     //????
 
-
-
-
-
-        //    ::capnp::List<Person>::Builder people = addressBook.initPeople(2);
-        //
-        //    Person::Builder alice = people[0];
-        //    alice.setId(123);
-        //    alice.setName("Alice");
-        //    alice.setEmail("alice@example.com");
-        //    // Type shown for explanation purposes; normally you'd use auto.
-        //    ::capnp::List<Person::PhoneNumber>::Builder alicePhones = alice.initPhones(1);
-        //    alicePhones[0].setNumber("555-1212");
-        //    alicePhones[0].setType(Person::PhoneNumber::Type::MOBILE);
-        //    alice.getEmployment().setSchool("MIT");
-        //
-        //    Person::Builder bob = people[1];
-        //    bob.setId(456);
-        //    bob.setName("Bob");
-        //    bob.setEmail("bob@example.com");
-        //    auto bobPhones = bob.initPhones(2);
-        //    bobPhones[0].setNumber("555-4567");
-        //    bobPhones[0].setType(Person::PhoneNumber::Type::HOME);
-        //    bobPhones[1].setNumber("555-7654");
-        //    bobPhones[1].setType(Person::PhoneNumber::Type::WORK);
-        //    bob.getEmployment().setUnemployed();
-        //
     writePackedMessageToFd(fd, message);
 
         zmq_msg_t msg;

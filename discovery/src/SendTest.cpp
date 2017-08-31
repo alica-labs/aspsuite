f2805c75db1af6a3641d8b25271d6f3a0f53359b
Stephan Opfer <opfer@vs.uni-kassel.de>
2017-08-31 18:05:37 +0200

    - further experimenting with zero-copy :(

--- discovery/src/SendTest.cpp
+++ discovery/src/SendTest.cpp
@@ -9,6 +9,7 @@
 
 #include <iostream>
 #include <unistd.h>
+#include <memory>
 
 /**
  * Checks the return code and reports an error if present.
@@ -24,41 +25,38 @@ void check(int returnCode, std::string methodName, bool abortIfError)
     }
 }
 
-int main(int argc, char **argv)
+void free (void * data, void * hint)
 {
-    auto ctx = zmq_ctx_new();
-    assert(ctx);
+}
 
-    auto socket = zmq_socket(ctx, ZMQ_RADIO);
-    check(zmq_connect(socket, "udp://224.0.0.1:5555"), "zmq_connect", true);
-
+void send(void * socket)
+{
     ::capnp::MallocMessageBuilder msgBuilder;
     discovery_msgs::Beacon::Builder beaconMsgBuilder = msgBuilder.initRoot<discovery_msgs::Beacon>();
 
     // set content
-    uuid_t uuid;
-    uuid_generate(uuid);
-
     beaconMsgBuilder.setIp("192.186.0.1");
     beaconMsgBuilder.setPort(6666);
+    uuid_t uuid;
+    uuid_generate(uuid);
     beaconMsgBuilder.setUuid(kj::arrayPtr(uuid, sizeof(uuid)));
 
     zmq_msg_t msg;
-    auto wordArray = capnp::messageToFlatArray(msgBuilder);
+    auto wordArray = std::shared_ptr<kj::Array<capnp::word>>(new kj::Array<capnp::word>(capnp::messageToFlatArray(msgBuilder)));
 
     std::cout << "Send Message: " << beaconMsgBuilder.toString().flatten().cStr() << std::endl;
 
-    check(zmq_msg_init_data(&msg, wordArray.begin(), wordArray.size() * sizeof(capnp::word), NULL, NULL),
+    check(zmq_msg_init_data(&msg, wordArray->begin(), wordArray->size() * sizeof(capnp::word), std::get_deleter<kj::Array<capnp::word>>(wordArray), NULL),
           "zmq_msg_init_data", false);
 
     /* uncomment for printing bytes */
-//    auto msgByteArray = reinterpret_cast<char *>(zmq_msg_data(&msg));
-//    for (int i = 0; i < zmq_msg_size(&msg); i++)
-//    {
-//    	std::cout << std::hex << msgByteArray[i] << ":";
-//
-//    }
-//    std::cout << std::endl;
+    //    auto msgByteArray = reinterpret_cast<char *>(zmq_msg_data(&msg));
+    //    for (int i = 0; i < zmq_msg_size(&msg); i++)
+    //    {
+    //    	std::cout << std::hex << msgByteArray[i] << ":";
+    //
+    //    }
+    //    std::cout << std::endl;
 
     // set group
     check(zmq_msg_set_group(&msg, "TestMCGroup"), "zmq_msg_set_group", false);
@@ -74,5 +72,18 @@ int main(int argc, char **argv)
     {
         std::cout << numBytesSend << " bytes sent." << std::endl;
     }
-    sleep(1);
+}
+
+int main(int argc, char **argv)
+{
+    auto ctx = zmq_ctx_new();
+    assert(ctx);
+
+    auto socket = zmq_socket(ctx, ZMQ_RADIO);
+    check(zmq_connect(socket, "udp://224.0.0.1:5555"), "zmq_connect", true);
+
+    send(socket);
+
+    check(zmq_close(socket), "zmq_close", true);
+    check(zmq_ctx_term(ctx), "zmq_ctx_term", true);
 }
